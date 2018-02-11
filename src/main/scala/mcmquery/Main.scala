bc4a5902c7caff8bee7db4c7801e3f66098c7167
Eduard <elohmann@xs4all.nl>
2018-02-11 15:38:50 +0100

    refactor + gitignore

--- src/main/scala/mcmquery/Main.scala
+++ src/main/scala/mcmquery/Main.scala
@@ -1,37 +1,39 @@
 package mcmquery
 
-case class Command(word: String, action : () => String ) { def execute() = action() }
+abstract case class Command(word : String) {
+
+  def execute() : Boolean
+
+  def register() = Command.doRegister(this)
+
+}
 
 object Command {
 
-  val all = new scala.collection.mutable.MutableList[Command]()
+  val all = scala.collection.mutable.ArrayBuffer[Command]()
 
-  def apply(word:String, action: () => String) = {
-    val c = new Command(word, action)
-    all += c
-    c
+  def doRegister(self: Command) : Unit = {
+    all += self
   }
 
 }
+
 
 object Main {
 
   def get_cmd() : Array[String] = scala.io.StdIn.readLine("> ").toLowerCase.trim.split("""\s+""").map(_.trim)
 
-  Command("help",    () => "Help text" )
-  Command("list",    () => Card.all.mkString("\n") )
-  Command("quit",    () => "Bye" )
-  Command("unknown", () => "I do not understand" )
-
-
+  new Command("help") { def execute() = { println("execute help "); false } }.register()
+  new Command("list") { def execute() = { println(Card.all.mkString("\n")); false} }.register()
+  new Command("quit") { def execute() = { println("Bye"); true } }.register()
 
   def run_shell() : Unit = {
-    while (true) {
+    var done = false
+    while (!done) {
       val line = get_cmd()
       Command.all.find(c => c.word == line(0)) match {
-        case None => println("I do not understand.")
-        case Some(c@Command("quit", _)) => { println(c.execute()); return }
-        case Some(c) => { println(c.execute()) }
+        case None    => println("I do not understand.")
+        case Some(c) => done = c.execute()
       }
     }
   }
